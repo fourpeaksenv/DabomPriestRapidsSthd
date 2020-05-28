61a7c725b5411e61a22fde87240b267f0ccee969
Kevin See <Kevin.See@merck.com>
2020-05-28 15:48:45 -0700

    indented some code

--- analysis/R_scripts/04_run_dabom.R
+++ analysis/R_scripts/04_run_dabom.R
@@ -26,136 +26,136 @@ yr = 2012
 for(yr in 2013:2019) {
   cat(paste('Working on', yr, '\n'))
 
-load(paste0('analysis/data/derived_data/PITcleanr/UC_Steelhead_', yr, '.rda'))
+  load(paste0('analysis/data/derived_data/PITcleanr/UC_Steelhead_', yr, '.rda'))
 
-proc_ch <- proc_list$ProcCapHist %>%
-  mutate_at(vars(UserProcStatus),
-            list(as.logical)) %>%
-  mutate(UserProcStatus = if_else(is.na(UserProcStatus),
-                                  AutoProcStatus,
-                                  UserProcStatus)) %>%
-  filter(UserProcStatus)
+  proc_ch <- proc_list$ProcCapHist %>%
+    mutate_at(vars(UserProcStatus),
+              list(as.logical)) %>%
+    mutate(UserProcStatus = if_else(is.na(UserProcStatus),
+                                    AutoProcStatus,
+                                    UserProcStatus)) %>%
+    filter(UserProcStatus)
 
-# file path to the default and initial model
-basic_modNm = 'analysis/model_files/PRD_DABOM.txt'
+  # file path to the default and initial model
+  basic_modNm = 'analysis/model_files/PRD_DABOM.txt'
 
-writeDABOM_PRA(file_name = basic_modNm)
+  writeDABOM_PRA(file_name = basic_modNm)
 
-#------------------------------------------------------------------------------
-# Alter default model code for species and year of
-# interest; sets prior for some detection node efficiencies at 0 or 100%
-# based on actual tag detection data; 0% if no tags were seen
-#------------------------------------------------------------------------------
+  #------------------------------------------------------------------------------
+  # Alter default model code for species and year of
+  # interest; sets prior for some detection node efficiencies at 0 or 100%
+  # based on actual tag detection data; 0% if no tags were seen
+  #------------------------------------------------------------------------------
 
-# filepath for specific JAGS model code for species and year
-mod_path = paste0('analysis/model_files/PRD_Steelhead_', yr, '.txt')
+  # filepath for specific JAGS model code for species and year
+  mod_path = paste0('analysis/model_files/PRD_Steelhead_', yr, '.txt')
 
-# writes species and year specific jags code
-fixNoFishNodes(basic_modNm,
-               mod_path,
-               proc_ch,
-               proc_list$NodeOrder)
+  # writes species and year specific jags code
+  fixNoFishNodes(basic_modNm,
+                 mod_path,
+                 proc_ch,
+                 proc_list$NodeOrder)
 
-#------------------------------------------------------------------------------
-# Create capture history matrices for each main branch to be used in
-# the JAGS data list
-#------------------------------------------------------------------------------
-# turn into wide version of capture histories, and add origin
-# add fish origin from biological data
-file_nms = list.files('analysis/data/derived_data')
-bio_nm = file_nms[grepl('Bio', file_nms) & grepl('.rds$', file_nms)]
+  #------------------------------------------------------------------------------
+  # Create capture history matrices for each main branch to be used in
+  # the JAGS data list
+  #------------------------------------------------------------------------------
+  # turn into wide version of capture histories, and add origin
+  # add fish origin from biological data
+  file_nms = list.files('analysis/data/derived_data')
+  bio_nm = file_nms[grepl('Bio', file_nms) & grepl('.rds$', file_nms)]
 
-bio_df = read_rds(paste0('analysis/data/derived_data/', bio_nm)) %>%
-  filter(Year == yr)
+  bio_df = read_rds(paste0('analysis/data/derived_data/', bio_nm)) %>%
+    filter(Year == yr)
 
-dabom_df = createDABOMcapHist(proc_ch %>%
-                                filter(TagID %in% bio_df$TagID),
-                              proc_list$NodeOrder,
-                              split_matrices = F) %>%
-  # add origin information
-  left_join(bio_df %>%
-              select(TagID, Origin) %>%
-              distinct()) %>%
-  select(TagID, Origin, everything())
-
-dabom_list = createDABOMcapHist(proc_ch %>%
+  dabom_df = createDABOMcapHist(proc_ch %>%
                                   filter(TagID %in% bio_df$TagID),
                                 proc_list$NodeOrder,
-                                split_matrices = T)
+                                split_matrices = F) %>%
+    # add origin information
+    left_join(bio_df %>%
+                select(TagID, Origin) %>%
+                distinct()) %>%
+    select(TagID, Origin, everything())
 
-dabom_list$fishOrigin = dabom_df %>%
-  mutate(Origin = recode(Origin,
-                         'W' = 1,
-                         'H' = 2)) %>%
-  pull(Origin)
+  dabom_list = createDABOMcapHist(proc_ch %>%
+                                    filter(TagID %in% bio_df$TagID),
+                                  proc_list$NodeOrder,
+                                  split_matrices = T)
 
-table(!is.na(dabom_list$fishOrigin))
+  dabom_list$fishOrigin = dabom_df %>%
+    mutate(Origin = recode(Origin,
+                           'W' = 1,
+                           'H' = 2)) %>%
+    pull(Origin)
 
-# Creates a function to spit out initial values for MCMC chains
-# n_branch_list = setBranchNums(parent_child)
-n_branch_list = setBranchNums_PRA()
+  table(!is.na(dabom_list$fishOrigin))
 
-init_fnc = setInitialValues_PRA(dabom_list)
+  # Creates a function to spit out initial values for MCMC chains
+  # n_branch_list = setBranchNums(parent_child)
+  n_branch_list = setBranchNums_PRA()
 
-# Create all the input data for the JAGS model
-jags_data = createJAGSinputs_PRA(dabom_list)
+  init_fnc = setInitialValues_PRA(dabom_list)
 
-#------------------------------------------------------------------------------
-# Tell JAGS which parameters in the model that it should save.
-# the fnc is hard coded and needs to be updated if there are changes!
-#------------------------------------------------------------------------------
+  # Create all the input data for the JAGS model
+  jags_data = createJAGSinputs_PRA(dabom_list)
 
-jags_params = setSavedParams(model_file = mod_path)
+  #------------------------------------------------------------------------------
+  # Tell JAGS which parameters in the model that it should save.
+  # the fnc is hard coded and needs to be updated if there are changes!
+  #------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
-# Run the model
+  jags_params = setSavedParams(model_file = mod_path)
 
-# Recommended MCMC parameters are:
-# * `n.chains`: 4
-# * `n.iter`: 5,000
-# * `n.burnin`: 2,500
-# * `n.thin`: 10
-# ( 4*(5000-2500) ) / 10 = 1000 samples
+  #------------------------------------------------------------------------------
+  # Run the model
 
-set.seed(12)
-dabom_mod <- jags.basic(data = jags_data,
-                        inits = init_fnc,
-                        parameters.to.save = jags_params,
-                        model.file = mod_path,
-                        n.chains = 4,
-                        n.iter = 5000,
-                        n.burnin = 2500,
-                        n.thin = 10,
-                        # n.chains = 1,
-                        # n.iter = 2,
-                        # n.burnin = 1,
-                        # parallel = T,
-                        DIC = T)
+  # Recommended MCMC parameters are:
+  # * `n.chains`: 4
+  # * `n.iter`: 5,000
+  # * `n.burnin`: 2,500
+  # * `n.thin`: 10
+  # ( 4*(5000-2500) ) / 10 = 1000 samples
 
-# set.seed(12)
-# # adaptation phase
-# jags = jags.model(mod_path,
-#                   data = jags_data,
-#                   inits = init_fnc,
-#                   n.chains = 4,
-#                   n.adapt = 1000)
-# # burn-in
-# update(jags,
-#        n.iter = 2500)
-# # posterior samples
-# dabom_mod = coda.samples(jags,
-#                          jags_params,
-#                          n.iter = 2500,
-#                          thin = 10)
+  set.seed(12)
+  dabom_mod <- jags.basic(data = jags_data,
+                          inits = init_fnc,
+                          parameters.to.save = jags_params,
+                          model.file = mod_path,
+                          n.chains = 4,
+                          n.iter = 5000,
+                          n.burnin = 2500,
+                          n.thin = 10,
+                          # n.chains = 1,
+                          # n.iter = 2,
+                          # n.burnin = 1,
+                          # parallel = T,
+                          DIC = T)
 
-# save some stuff
-proc_list[["proc_ch"]] <- proc_ch
+  # set.seed(12)
+  # # adaptation phase
+  # jags = jags.model(mod_path,
+  #                   data = jags_data,
+  #                   inits = init_fnc,
+  #                   n.chains = 4,
+  #                   n.adapt = 1000)
+  # # burn-in
+  # update(jags,
+  #        n.iter = 2500)
+  # # posterior samples
+  # dabom_mod = coda.samples(jags,
+  #                          jags_params,
+  #                          n.iter = 2500,
+  #                          thin = 10)
 
-save(dabom_mod, dabom_list, proc_list, parent_child, bio_df,
-     file = paste0("analysis/data/derived_data/model_fits/PRA_Steelhead_", yr,'_DABOM.rda'))
+  # save some stuff
+  proc_list[["proc_ch"]] <- proc_ch
 
-rm(dabom_mod, dabom_list, proc_list, parent_child, bio_df,
-   proc_ch, jags_data, init_fnc, mod_path, )
+  save(dabom_mod, dabom_list, proc_list, parent_child, bio_df,
+       file = paste0("analysis/data/derived_data/model_fits/PRA_Steelhead_", yr,'_DABOM.rda'))
+
+  rm(dabom_mod, dabom_list, proc_list, parent_child, bio_df,
+     proc_ch, jags_data, init_fnc, mod_path, )
 
 }
 
